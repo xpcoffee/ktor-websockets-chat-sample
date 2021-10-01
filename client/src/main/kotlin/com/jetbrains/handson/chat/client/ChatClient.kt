98ba04aa66ba2a2e38e133b425ab98fcf31432d7
Emerick Bosch <xpc.dev@gmail.com>
2021-10-01 17:09:44 +0200

    Go through basic tutorial; make tweaks to get to running
    Tweaks:
     - gradle version needed to be updated to allow it to run with openjdk 16
     - I'm running this in WSL2; the Windows system has trouble connecting websockets to
       the Linux network. Instead use websocat in WSL or go skip testing the server (go
       straight to the cli client part of the tutorial).
    
    Tutorial URL: https://ktor.io/docs/creating-web-socket-chat.html

--- client/src/main/kotlin/com/jetbrains/handson/chat/client/ChatClient.kt
+++ client/src/main/kotlin/com/jetbrains/handson/chat/client/ChatClient.kt
@@ -9,5 +9,40 @@ import kotlinx.coroutines.*
 
 @KtorExperimentalAPI
 fun main() {
+    val client = HttpClient {
+        install(WebSockets)
+    }
+    runBlocking {
+        client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
+            val messageOutputRoutine = launch { outputMessages() }
+            val userInputRoutine = launch { inputMessages() }
+            userInputRoutine.join()
+            messageOutputRoutine.cancelAndJoin()
+        }
+    }
+    client.close()
+    println("Connection closed! Goodbye!")
+}
 
+suspend fun DefaultWebSocketSession.outputMessages() {
+   try {
+       for(message in incoming) {
+           message as? Frame.Text ?: continue
+           println(message.readText())
+       }
+   } catch(e: Exception) {
+       println("Error while receiving: ${e.localizedMessage}")
+   }
+}
+
+suspend fun DefaultWebSocketSession.inputMessages() {
+    while(true) {
+        val message = readLine()
+        if("exit".equals(message, true)) return
+        try {
+            if(message!= null) send(message)
+        } catch (e: Exception) {
+            println("Error while sending: ${e.localizedMessage}")
+        }
+    }
 }
